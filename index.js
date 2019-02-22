80838d7bc45a189897c942bf8af27a01c18a4c78
Saumya Bajracharya <ssbajracharya@hotmail.com>
2019-02-22 18:56:48 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,9 +7,9 @@ function whisper(string){
 }
 
 function logShout(string){
-  return console.log(string.toUpperCase());
+  console.log(string.toUpperCase());
 }
 
 function logWhisper(string){
-  return console.log(string.toLowerCase());
+  console.log(string.toLowerCase());
 }
